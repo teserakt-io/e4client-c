882dc24a57db529f0d5db9efc906dc2d45de18ba
Antony Vennard <antony@vennard.ch>
2019-03-05 16:18:55 +0100

    Remove everything that has been moved to the library.

---
+++ src/e4cli.c
@@ -0,0 +1,23 @@
+/* E4 Client Code in C
+  
+  (C) 2018-2019 Copyright Teserakt AG, Lausanne, Switzerland.
+
+  2018-05-01  Markku-Juhani O. Saarinen <markku@teserakt.io
+  2019-01-01  Antony Vennard <antony@teserakt.io>
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <time.h>
+
+#include "MQTTClient.h"
+
+/* Use the file storage command */
+#define E4_STORE_FILE
+#include "e4/e4.h"
+
+int main(int argc, char** argv) {
+
+}
